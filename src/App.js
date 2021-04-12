7dc938ef4b4105fc4da21650cb8e1cbae1d44975
cogito1016 <cogito1016@naver.com>
2021-04-12 16:50:47 +0900

    feat: Page Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Interiors from "./pages/Interiors";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Interiors />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Interiors.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+  width: 1600px;
+  margin: 30px auto 0 auto;
+  background-color: gray;
+`;
+
+const Interiors = () => {
+  return <Container>Im Interiors</Container>;
+};
+
+export default Interiors;
